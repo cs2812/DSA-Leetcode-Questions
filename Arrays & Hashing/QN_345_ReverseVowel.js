dac87bcfbd969ef2d8ea50a17ce94291961f58db
cs2812 <cs786786786@gmail.com>
2023-01-24 22:12:58 +0530

    reverseVowels(s1)

---
+++ Arrays & Hashing/QN_345_ReverseVowel.js
@@ -0,0 +1,22 @@
+
+var reverseVowels = function(s) {
+    let str=s.split("") 
+    let stk = []
+    let vowel = ['a','e','i',"o","u",'A','E','I',"O","U"]
+    for(let ele of str){
+        if(vowel.includes(ele)){
+            stk.push(ele)
+        }
+    }
+    for(let i=0;i<str.length;i++){
+        if(vowel.includes(str[i])){
+            let a = stk.pop()
+            str[i]=a
+        }
+    }
+    console.log(str.join("")) 
+ };
+ let s1 = "hello"
+ let s2 = "leetcode"
+ reverseVowels(s1)
+ reverseVowels(s2)
