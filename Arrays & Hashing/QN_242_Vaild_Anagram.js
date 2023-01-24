d45fe47599b7eb128e6fcfb6299f5828c89b5928
cs2812 <cs786786786@gmail.com>
2023-01-24 22:21:28 +0530

    console.log(isAnagram(s1,t1))

---
+++ Arrays & Hashing/QN_242_Vaild_Anagram.js
@@ -0,0 +1,23 @@
+var isAnagram = function(s, t) {
+    if(s.length!==t.length){return false}
+    let o={}
+    let h={}
+    for(let i of s ){
+        o[i]=(o[i]||0)+1
+    }
+    for(let i of t){
+        h[i]=(h[i]||0)+1
+        
+    }
+    for(let i in h){
+        if(!o[i] || o[i]!==h[i]){
+            return false
+        }
+    }
+    return true 
+};
+
+let s1 = "rat", t1 = "car"
+let s2 = "anagram", t2 = "nagaram"
+console.log(isAnagram(s1,t1))
+console.log(isAnagram(s2,t2))
