7fd870d25dceccc04b680bbb691d3f55a416774b
cs2812 <cs786786786@gmail.com>
2023-01-31 00:34:29 +0530

    up Prefix

--- Arrays & Hashing/QN_14_Longest_Common_Prefix.js
+++ Arrays & Hashing/QN_14_Longest_Common_Prefix.js
@@ -1,18 +1,24 @@
+/*
+The thing is with js we can sort strings and when we sort an array of different strings they will be sorted alphabetically, 
+which means all we left to do is find the common part between the first and the last values in the sorted array. 
+As simple as this.
+*/
 var longestCommonPrefix = function(strs) {
-    if(strs.length==1){
-        return strs[0]
+strs.sort()
+  for(let i=0;i<strs[0].length;i++){
+    if(strs[0][i]!==strs[strs.length-1][i]){
+        return strs[0].slice(0,i)
     }
-let prefix=""
-  for(let i=0;i<strs.length;i++){
-    let char = strs[0][i]
-    for(let j=1;j<strs.length;j++){
-      if(char!==strs[j][i]){return prefix}
-    }
-      prefix+=char
   }
-    
-}
+  return strs[0];
+};
 let strs1 = ["flower","flow","flight"]
 let strs2 = ["dog","racecar","car"]
+let strs3 = ["aa","aaa","aa"]
+let strs4=[""]
+let strs5=["","",""]
 console.log(longestCommonPrefix(strs1))
 console.log(longestCommonPrefix(strs2))
+console.log(longestCommonPrefix(strs3))
+console.log(longestCommonPrefix(strs4))
+console.log(longestCommonPrefix(strs5))
