bdd88923e52dc8ccf510fe7ed4cc8eacc04e6409
cs2812 <cs786786786@gmail.com>
2023-03-04 17:45:52 +0530

    plusOne

---
+++ Arrays & Hashing/QN_66_plus_one.js
@@ -0,0 +1,18 @@
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] === 9) {
+      digits[i] = 0;
+    } else {
+      digits[i] += 1;
+      return digits;
+    }
+  }
+  if (digits[0] === 0) {
+    digits.unshift(1);
+    return digits;
+  }
+};
+
+console.log(plusOne([1,2,3]))
+console.log(plusOne([9,9,9]))
+console.log(plusOne([9,0,9]))
